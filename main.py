1f957abc3b2895f11b4c82549d93509fd7dbb566
Sanaa514 <sanaamontgomery10@gmail.com>
2020-08-04 18:23:34 +0000

    psuedo code

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import pygame
 from paddle import Paddle
 from ball import Ball
 
-pygame.init()
-Paddle.init() 
-Ball.init() 
+pygame.init() 
 
 #define colors
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 
-size = (100,15)
+size = (700,500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
 
@@ -45,7 +43,7 @@ clock = pygame.time.Clock()
 scoreS = 0
 scoreT = 0 
 
-
+#Main loop 
 while carryOn:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -53,7 +51,7 @@ while carryOn:
     elif event.type==pygame.KEYDOWN:
      if event.key==pygame.K_x:
         carryOn=False 
-
+##Moving the paddles when in use 
   keys = pygame.key.get_pressed()       
   if keys[pygame.K_w]:
     paddleS.moveup(5)
@@ -66,12 +64,12 @@ while carryOn:
 
 all_sprites_list.update()       
 
-#ball movement
-if ball.rect.x>=300:
+#ball movement, see if the ball bounces against the walls 
+if ball.rect.x>=690:
   ball.velocity[0] = -ball.velocity[0]
 if ball.rect.x<=0:
   ball.velocity[0] = -ball.velocity[0]  
-if ball.rect.y>250:
+if ball.rect.y>490:
   ball.velocity[1] = -ball.velocity[1]   
 if ball.rect.y<0:
   ball.velocity[1] = -ball.velocity[1]  
@@ -80,22 +78,23 @@ if ball.rect.y<0:
 if pygame.sprite.collide_mask(ball, paddleS) or pygame.sprite.collide_mask(ball, paddleT):
   ball.bounce()
   
-
+#Drawing code 
 screen.fill(BLACK)
-pygame.draw.line(screen, WHITE,[300,0], [300,200],5)   
+pygame.draw.line(screen, WHITE,[349,0], [349,500],5)   
 
+#sprites
 all_sprites_list.draw(screen)
 
 #adding score
-font = pygame.font.Font(None, 54)
+font = pygame.font.Font(None, 74)
 text = font.render(str(scoreS), 1, WHITE)
 screen.blit(text,(250,10))
 text = font.render(str(scoreT), 1, WHITE)
 screen.blit(text, (420,10))
 
 pygame.display.flip()
+
 #add time frame for each game played
 clock.tick(60)
 
 pygame.quit()
-
